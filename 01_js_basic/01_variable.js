f1308d221df5852d765b954c579518f9ebde60f1
CSEAnkit <167836774+CSEAnkit@users.noreply.github.com>
2024-04-24 14:33:17 +0000

    commit

---
+++ 01_js_basic/01_variable.js
@@ -0,0 +1,9 @@
+const Name= "Ankit"
+let Email ="Ankit123@gmail.com"
+var Password  = "12321"
+state = "UP"
+Email = "Babaaji@gmail.com"
+Password = "!2112"
+state = "Bihar"
+console.log(Name)
+console.table([Email , Password, state , Name])
---
+++ 01_js_basic/02_datatype.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all code as newew version 
+// alert(3+3 ) // we are using nodejs, not browser 
+
+// number => 2 to power 53
+
+// bigint
+// string ""
+// boolean => true/false
+//null=> standalone value  (it is empty )
+// undefined => value is not defined 
+// symbol => used when we define unique
+let age =23
+console.table([typeof(age),typeof null, typeof undefined ])
