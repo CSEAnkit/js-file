7f40509360d2f656eacbbfbd72f1c26a29d3efc8
CSEAnkit <167836774+CSEAnkit@users.noreply.github.com>
2024-05-08 10:17:53 +0000

    conversion

---
+++ 01_js_basic/03_Conversion_Operation.js
@@ -0,0 +1,45 @@
+// Note :> be focous on the type of number and what is stored in that and what happen after the conversion
+
+// conversion into number 
+let var1 = "232a"
+let var2 ="23"
+let var3 = null
+let var4 =undefined
+let var5 = true 
+let var6 = "Ankit"
+
+console.log(typeof(var1))
+let var1InNumber = Number(var1) // way of converting into a number 
+
+console.log(typeof(var1InNumber))
+console.log(var1InNumber) // Here it is not na number bcz in var1 the value is 232a
+
+let var2InNumber = Number(var2) 
+console.log(var2InNumber) // Here it is  number bcz in var2 the value is 23 so the number is 23
+
+let var3InNumber = Number(var3) 
+console.log(var3InNumber)   // it will print 0
+
+let var4InNumber = Number(var4) 
+console.log(var4InNumber) // it will print NaN
+
+let var5InNumber = Number(var5) 
+console.log(var5InNumber) // it will print 1
+
+let var6InNumber = Number(var6) 
+console.log(var6InNumber) // it will print NaN
+
+// To convert into the boolean just use Boolean at the place of Number 
+// At the time of boolean when empty then it will print false
+// 1=>true; 0=>false 
+// "" => false
+// "Ankit" => true
+
+
+// To convert into the string just use String at the place of Number 
+
+let str1= "@12"
+console.log(typeof str1)
+let strInString = String(str1)
+console.log(strInString) 
+console.log( typeof strInString) 
