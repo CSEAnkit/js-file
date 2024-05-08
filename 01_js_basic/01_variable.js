5bf3ccd6558910172a1a36be7fd7a5eed6963073
CSEAnkit <167836774+CSEAnkit@users.noreply.github.com>
2024-05-08 09:18:03 +0000

    notes are avilable in form of comment

--- 01_js_basic/01_variable.js
+++ 01_js_basic/01_variable.js
@@ -1,9 +1,21 @@
-const Name= "Ankit"
+// to run this write node then file name 
+// const is used  when we do not want to change the value 
+const Name= "Ankit" // can't be changed bcz it is const 
 let Email ="Ankit123@gmail.com"
 var Password  = "12321"
+/*
+var is not prefere to used 
+bcz of issue in block scope and functional scope 
+
+*/ 
 state = "UP"
 Email = "Babaaji@gmail.com"
 Password = "!2112"
 state = "Bihar"
+ let var1;
+// console.log me jo bhi likenge as it is print hoga 
 console.log(Name)
-console.table([Email , Password, state , Name])
+// Another way to print 
+
+console.table([Email , Password, state , Name,var1])
+// var1 value is undefined 
