7484ccc9ebac71bb380a7cf09c7d2f5778808cb3
CSEAnkit <167836774+CSEAnkit@users.noreply.github.com>
2024-05-08 09:43:11 +0000

    commit

--- 01_js_basic/02_datatype.js
+++ 01_js_basic/02_datatype.js
@@ -1,14 +1,24 @@
-"use strict"; // treat all code as newew version 
-// alert(3+3 ) // we are using nodejs, not browser 
+"use strict"; // treat all code as newer version of code 
+
+// alert(3+3 )  we are using nodejs, not browser it will  show the error
+
+// Ecma script is standard for JS
+
+// all is the data type in JavaScript
 
 // number => 2 to power 53
-
 // bigint
 // string ""
 // boolean => true/false
 //null=> standalone value  (it is empty )
 // undefined => value is not defined 
-// symbol => used when we define unique
+// symbol => used when we have to define unique
 let age =23
 console.table([typeof(age),typeof null, typeof undefined ]) 
 console.log(age)
+// object 
+// typeof is used to know the type of variable
+
+/*  type of null is obj
+    type of undefined is undefined
+*/
