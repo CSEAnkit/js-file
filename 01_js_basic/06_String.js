c61d4e2fd3bbb0a6b71cb75d229ef50a1f9fd177
CSEAnkit <167836774+CSEAnkit@users.noreply.github.com>
2024-05-11 11:53:42 +0000

    commit

--- 01_js_basic/06_String.js
+++ 01_js_basic/06_String.js
@@ -9,17 +9,31 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount }`);
 
 
 // Another way to declare string 
-const gameName = new String('AnkitBaba') // basically key value pair go on console and write the code and enter
+const gameName = new String('Ankit-Pandey') // basically key value pair go on console and write the code and enter
 console.log(gameName[0])
 console.log(gameName.__proto__);
-console.log(gameName.length);
+console.log(gameName.length);  // It will print the length.
 console.log(gameName.toUpperCase()); // print in Upper case 
 console.log(gameName.charAt(2)); // print which character is at index 2
-console.log(gameName.charOf('t')); // print character index
+console.log(gameName.indexOf('t')); // print character index
 
 const newString = gameName.substring(0,4)// It will print the string at index 0 to 4  negative value dene per bhi 0 se hi  start karta hai
 console.log(newString); 
 
 const anotherString = gameName.slice(-8, 4) // can give negative index
 console.log(anotherString)
+
+const newString2 = "  Ankit "
+console.log(newString2)
+console.log(newString2.trim()) // remove all the space
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-')) // it search in the url and replace the givin value  which is written in the code 
+
+console.log(url.includes('sundar')) // It will search the key word in the url
+
+console.log(gameName.split('-'));// It will seprate on the basic of given code  result ->[ 'Ankit', 'Pandey' ].
+
+
 
